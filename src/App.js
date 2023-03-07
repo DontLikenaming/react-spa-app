f1e72e25cbe617316f50dc8ac58bdb7333b475e5
DontLikenaming <edirna21@gmail.com>
2023-03-07 12:51:33 +0900

    NavLink로 변경, NotFound 추가 외 수정

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import Home from './pages/Home';
 import Stuff from './pages/Stuff';
 import Contact from './pages/Contact';
-import {Route, Routes} from "react-router-dom";
+import NotFound from './pages/NotFound';
+import {NavLink, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
       <div>
         <h1>Simple SPA</h1>
         <ul className="header">
-            <li><a href="/">Home</a></li>
-            <li><a href="/Stuff">Stuff</a></li>
-            <li><a href="/Contact">Contact</a></li>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/Stuff">Stuff</NavLink></li>
+            <li><NavLink to="/Contact">Contact</NavLink></li>
         </ul>
         <div className="content">
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/Stuff" element={<Stuff />} />
                 <Route path="/Contact" element={<Contact />} />
+                <Route path="/*" element={<NotFound />} />
             </Routes>
         </div>
       </div>
